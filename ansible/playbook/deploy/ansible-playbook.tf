21673aa463231f4acf11a3fcab177a517052f0c7
Zeta <zeta@nomocode.io>
2023-08-23 09:49:25 +0000

    Update

---
+++ ansible/playbook/deploy/ansible-playbook.tf
@@ -0,0 +1,16 @@
+resource "local_sensitive_file" "id_rsa" {
+  filename = "id_rsa"
+  content  = local.ssh_key.private_key_pem
+}
+
+resource "null_resource" "web_config" {
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "/usr/local/bin/ansible-playbook -i '${local.server_ipv4_address},' -u root --private-key id_rsa playbook.yaml"
+    quiet = true
+  }
+}
